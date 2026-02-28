28d5445e060aca82ae5e8e12cdd8a693c1660e5a
DPNam1510 <dpnbhd2k@gmail.com>
2026-02-28 19:42:43 +0700

    nam up code

---
+++ case_study_m4_team1/src/test/java/com/example/case_study_m4_team1/CaseStudyM4Team1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.case_study_m4_team1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseStudyM4Team1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
